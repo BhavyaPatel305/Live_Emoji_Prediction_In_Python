0e32bca2457d6bf0bb0e935f51deed60b0bd5e0a
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2024-02-29 02:36:13 +0000

    Drawing Landmarks on Frame

--- Data_Collection.py
+++ Data_Collection.py
@@ -6,9 +6,35 @@ import cv2
 # Take video feed as the input
 cap = cv2.VideoCapture(0)
 
+# Using Mediapipe Library
+
+# Takes in frames and returns facial keypoints like left hand, right hand, body etc.
+holistic = mp.solutions.holistic
+
+# To show visuals
+hands = mp.solutions.hands
+
+# Object of holistic class
+holis = holistic.Holistic()
+
+# To show visuals
+drawing = mp.solutions.drawing_utils
+
 while True:
     # Read the feed
     _, frm = cap.read()
+    
+    # Flip the frame to avoid mirror effect(Flip left to right, so flip code 1)
+    frm = cv2.flip(frm, 1)
+    
+    # Convert frame from cvtColor to RGB and pass it to the holistic object
+    res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+    
+    # Draw on the frame: face landmarks, left_hand landmarks, right_hand landmarks
+    drawing.draw_landmarks(frm, res.face_landmarks)
+    drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+    drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+    
     
     # Show it to user
     cv2.imshow("window", frm)
